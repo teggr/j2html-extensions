7bc2ae0096cbacf7073de6e5bb8d6f90c967159b
teggr <robin@tegg.me.uk>
2024-10-08 13:36:51 +0100

    feat(boostrap) badge

---
+++ bootstrap-j2html-extension/src/main/java/dev/rebelcraft/j2html/ext/components/Badge.java
@@ -0,0 +1,9 @@
+package dev.rebelcraft.j2html.ext.components;
+
+public class Badge {
+
+    public static final String badge = "badge";
+
+    public static final String rounded_pill = "rounded-pill";
+
+}
