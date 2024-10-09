63caabed1ffa23866fee49947071e73e3e118719
teggr <robin@tegg.me.uk>
2024-10-09 08:46:04 +0100

    feat(boostrap) card

---
+++ bootstrap-j2html-extension/src/main/java/dev/rebelcraft/j2html/ext/components/Card.java
@@ -0,0 +1,41 @@
+package dev.rebelcraft.j2html.ext.components;
+
+import j2html.attributes.Attribute;
+
+import static dev.rebelcraft.j2html.ext.components.BreadCrumb.aria_label;
+
+public class Card {
+
+    public static final String card = "card";
+
+    public static final String card_img_top = "card-img-top";
+    public static final String card_img_bottom = "card-img-bottom";
+    public static final String card_img = "card-img";
+    public static final String card_img_overlay = "card-img-overlay";
+
+    public static final String card_body = "card-body";
+
+    public static final String card_title = "card-title";
+
+    public static final String card_text = "card-text";
+
+    public static final String card_link = "card-link";
+
+    public static final String card_subtitle = "card-subtitle";
+
+    public static final String list_group = "list-group";
+
+    public static final String list_group_flush = "list-group-flush";
+
+    public static final String card_header = "card-header";
+
+    public static final String card_footer = "card-footer";
+
+    public static final String bg_transparent = "bg-transparent";
+
+    public static final String card_group = "card-group";
+
+    public static final String h_100 = "h-100";
+
+
+}
