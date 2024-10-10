def2483b9c1e033d411ceb75cb8dd2daba3a66dd
teggr <robin@tegg.me.uk>
2024-10-10 19:59:00 +0100

    feat(boostrap) listgroup

---
+++ bootstrap-j2html-extension/src/main/java/dev/rebelcraft/j2html/ext/components/ListGroup.java
@@ -0,0 +1,47 @@
+package dev.rebelcraft.j2html.ext.components;
+
+import j2html.attributes.Attribute;
+
+public class ListGroup {
+
+    public static final String list_group = "list-group";
+
+    public static final String list_group_item = "list-group-item";
+
+    public static final String active = "active";
+
+    public static final String disabled = "disabled";
+
+    public static final String list_group_item_action = "list-group-item-action";
+
+    public static final String list_group_flush = "list-group-flush";
+    public static final String list_group_numbered = "list-group-numbered";
+
+    public static final String list_group_horizontal = "list-group-horizontal";
+
+    public static final String list_group_horizontal_sm = "list-group-horizontal-sm";
+    public static final String list_group_horizontal_md = "list-group-horizontal-md";
+    public static final String list_group_horizontal_lg = "list-group-horizontal-lg";
+    public static final String list_group_horizontal_xl = "list-group-horizontal-xl";
+    public static final String list_group_horizontal_xxl = "list-group-horizontal-xxl";
+
+    public static final String list_group_primary = "list-group-item-primary";
+    public static final String list_group_secondary = "list-group-item-secondary";
+    public static final String list_group_success = "list-group-item-success";
+    public static final String list_group_danger = "list-group-item-danger";
+    public static final String list_group_warning = "list-group-item-warning";
+    public static final String list_group_info = "list-group-item-info";
+    public static final String list_group_light = "list-group-item-light";
+    public static final String list_group_dark = "list-group-item-dark";
+
+    public static Attribute ariaCurrentTrue() {
+        return new Attribute(aria_current, "true");
+    }
+
+    public static final String aria_current = "aria-current";
+    public static final String aria_disabled = "aria-disabled";
+
+    public static Attribute ariaDisabledTrue() {
+        return new Attribute(aria_disabled, "true");
+    }
+}
