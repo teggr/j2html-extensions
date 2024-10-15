b036cb0438381e9f1ffcfa0614290c31fa335967
teggr <robin@tegg.me.uk>
2024-10-15 11:25:50 +0100

    feat(boostrap) spinners

---
+++ bootstrap-j2html-extension/src/main/java/dev/rebelcraft/j2html/ext/components/Spinners.java
@@ -0,0 +1,30 @@
+package dev.rebelcraft.j2html.ext.components;
+
+import j2html.TagCreator;
+import j2html.attributes.Attribute;
+import j2html.tags.specialized.SpanTag;
+
+public class Spinners {
+
+    public static final String spinner_border = "spinner-border";
+
+    public static final String visually_hidden = "visually-hidden";
+
+    public static final String spinner_grow = "spinner-grow";
+
+    public static final String spinner_border_sm = "spinner-border-sm";
+    public static final String spinner_grow_sm = "spinner-grow-sm";
+
+    public static Attribute roleStatus() {
+        return new Attribute("role", "status");
+    }
+
+    public static SpanTag loader() {
+        return TagCreator.span("Loading...").withClass(visually_hidden);
+    }
+
+    public static Attribute ariaHiddenTrue() {
+        return new Attribute("aria-hidden", "true");
+    }
+
+}
