9020e92ac1e7240117a9fc384acf35827d8dee40
teggr <robin@tegg.me.uk>
2024-10-16 16:49:11 +0100

    feat(boostrap) interactions

---
+++ bootstrap-j2html-extension/src/main/java/dev/rebelcraft/j2html/ext/utilities/Interactions.java
@@ -0,0 +1,12 @@
+package dev.rebelcraft.j2html.ext.utilities;
+
+public class Interactions {
+
+    public static final String user_select_all = "user-select-all";
+    public static final String user_select_auto = "user-select-auto";
+    public static final String user_select_none = "user-select-none";
+
+    public static final String pe_auto = "pe-auto";
+    public static final String pe_none = "pe-none";
+
+}
