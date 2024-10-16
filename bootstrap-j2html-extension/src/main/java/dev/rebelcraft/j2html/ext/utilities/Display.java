2a4cbaf103110991b6747fac18283bd27437a66c
teggr <robin@tegg.me.uk>
2024-10-16 13:26:58 +0100

    feat(boostrap) display

---
+++ bootstrap-j2html-extension/src/main/java/dev/rebelcraft/j2html/ext/utilities/Display.java
@@ -0,0 +1,104 @@
+package dev.rebelcraft.j2html.ext.utilities;
+
+public class Display {
+
+    public static final String d_none = "d-none";
+    public static final String d_inline = "d-inline";
+    public static final String d_inline_block = "d-inline-block";
+    public static final String d_block = "d-block";
+    public static final String d_grid = "d-grid";
+    public static final String d_inline_grid = "d-inline-grid";
+    public static final String d_table = "d-table";
+    public static final String d_table_cell = "d-table-cell";
+    public static final String d_table_row = "d-table-row";
+    public static final String d_flex = "d-flex";
+    public static final String d_inline_flex = "d-inline-flex";
+
+    public static final String d_sm_none = "d-sm-none";
+    public static final String d_sm_inline = "d-sm-inline";
+    public static final String d_sm_inline_block = "d-sm-inline-block";
+    public static final String d_sm_block = "d-sm-block";
+    public static final String d_sm_grid = "d-sm-grid";
+    public static final String d_sm_inline_grid = "d-sm-inline-grid";
+    public static final String d_sm_table = "d-sm-table";
+    public static final String d_sm_table_cell = "d-sm-table-cell";
+    public static final String d_sm_table_row = "d-sm-table-row";
+    public static final String d_sm_flex = "d-sm-flex";
+    public static final String d_sm_inline_flex = "d-sm-inline-flex";
+
+    public static final String d_md_none = "d-md-none";
+    public static final String d_md_inline = "d-md-inline";
+    public static final String d_md_inline_block = "d-md-inline-block";
+    public static final String d_md_block = "d-md-block";
+    public static final String d_md_grid = "d-md-grid";
+    public static final String d_md_inline_grid = "d-md-inline-grid";
+    public static final String d_md_table = "d-md-table";
+    public static final String d_md_table_cell = "d-md-table-cell";
+    public static final String d_md_table_row = "d-md-table-row";
+    public static final String d_md_flex = "d-md-flex";
+    public static final String d_md_inline_flex = "d-md-inline-flex";
+
+    public static final String d_lg_none = "d-lg-none";
+    public static final String d_lg_inline = "d-lg-inline";
+    public static final String d_lg_inline_block = "d-lg-inline-block";
+    public static final String d_lg_block = "d-lg-block";
+    public static final String d_lg_grid = "d-lg-grid";
+    public static final String d_lg_inline_grid = "d-lg-inline-grid";
+    public static final String d_lg_table = "d-lg-table";
+    public static final String d_lg_table_cell = "d-lg-table-cell";
+    public static final String d_lg_table_row = "d-lg-table-row";
+    public static final String d_lg_flex = "d-lg-flex";
+    public static final String d_lg_inline_flex = "d-lg-inline-flex";
+
+    public static final String d_xl_none = "d-xl-none";
+    public static final String d_xl_inline = "d-xl-inline";
+    public static final String d_xl_inline_block = "d-xl-inline-block";
+    public static final String d_xl_block = "d-xl-block";
+    public static final String d_xl_grid = "d-xl-grid";
+    public static final String d_xl_inline_grid = "d-xl-inline-grid";
+    public static final String d_xl_table = "d-xl-table";
+    public static final String d_xl_table_cell = "d-xl-table-cell";
+    public static final String d_xl_table_row = "d-xl-table-row";
+    public static final String d_xl_flex = "d-xl-flex";
+    public static final String d_xl_inline_flex = "d-xl-inline-flex";
+
+    public static final String d_xxl_none = "d-xxl-none";
+    public static final String d_xxl_inline = "d-xxl-inline";
+    public static final String d_xxl_inline_block = "d-xxl-inline-block";
+    public static final String d_xxl_block = "d-xxl-block";
+    public static final String d_xxl_grid = "d-xxl-grid";
+    public static final String d_xxl_inline_grid = "d-xxl-inline-grid";
+    public static final String d_xxl_table = "d-xxl-table";
+    public static final String d_xxl_table_cell = "d-xxl-table-cell";
+    public static final String d_xxl_table_row = "d-xxl-table-row";
+    public static final String d_xxl_flex = "d-xxl-flex";
+    public static final String d_xxl_inline_flex = "d-xxl-inline-flex";
+
+    public static final String hidden_on_all = "d-none";
+    public static final String hidden_only_on_s = "d-none d-sm-block";
+    public static final String hidden_only_on_sm = "d-sm-none d-md-block";
+    public static final String hidden_only_on_md = "d-md-none d-lg-block";
+    public static final String hidden_only_on_lg = "d-lg-none d-xl-block";
+    public static final String hidden_only_on_xl = "d-xl-none d-xxl-block";
+    public static final String hidden_only_on_xxl = "d-xxl-none";
+    public static final String visible_on_all = "d-block";
+    public static final String visible_only_on_xs = "d-block d-sm-none";
+    public static final String visible_only_on_sm = "d-none d-sm-block d-md-none";
+    public static final String visible_only_on_md = "d-none d-md-block d-lg-none";
+    public static final String visible_only_on_lg = "d-none d-lg-block d-xl-none";
+    public static final String visible_only_on_xl = "d-none d-xl-block d-xxl-none";
+    public static final String visible_only_on_xxl = "d-none d-xxl-block";
+
+    public static final String d_print_none = "d-print-none";
+    public static final String d_print_inline = "d-print-inline";
+    public static final String d_print_inline_block = "d-print-inline-block";
+    public static final String d_print_block = "d-print-block";
+    public static final String d_print_grid = "d-print-grid";
+    public static final String d_print_inline_grid = "d-print-inline-grid";
+    public static final String d_print_table = "d-print-table";
+    public static final String d_print_table_cell = "d-print-table-cell";
+    public static final String d_print_table_row = "d-print-table-row";
+    public static final String d_print_flex = "d-print-flex";
+    public static final String d_print_inline_flex = "d-print-inline-flex";
+
+}
