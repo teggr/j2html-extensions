48768c24f92295fb477d82528e88b806c2ff70cb
teggr <robin@tegg.me.uk>
2024-10-16 16:45:17 +0100

    feat(boostrap) float

---
+++ bootstrap-j2html-extension/src/main/java/dev/rebelcraft/j2html/ext/utilities/Float.java
@@ -0,0 +1,24 @@
+package dev.rebelcraft.j2html.ext.utilities;
+
+public class Float {
+
+    public static final String float_start = "float-start";
+    public static final String float_end = "float-end";
+    public static final String float_none = "float-none";
+    public static final String float_sm_start = "float-sm-start";
+    public static final String float_sm_end = "float-sm-end";
+    public static final String float_sm_none = "float-sm-none";
+    public static final String float_md_start = "float-md-start";
+    public static final String float_md_end = "float-md-end";
+    public static final String float_md_none = "float-md-none";
+    public static final String float_lg_start = "float-lg-start";
+    public static final String float_lg_end = "float-lg-end";
+    public static final String float_lg_none = "float-lg-none";
+    public static final String float_xl_start = "float-xl-start";
+    public static final String float_xl_end = "float-xl-end";
+    public static final String float_xl_none = "float-xl-none";
+    public static final String float_xxl_start = "float-xxl-start";
+    public static final String float_xxl_end = "float-xxl-end";
+    public static final String float_xxl_none = "float-xxl-none";
+
+}
