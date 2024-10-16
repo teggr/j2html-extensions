c44a78d0910a7616fd752f087b191e3d25e030ae
teggr <robin@tegg.me.uk>
2024-10-16 12:07:24 +0100

    feat(boostrap) background

---
+++ bootstrap-j2html-extension/src/main/java/dev/rebelcraft/j2html/ext/utilities/Background.java
@@ -0,0 +1,35 @@
+package dev.rebelcraft.j2html.ext.utilities;
+
+public class Background {
+
+    public static final String bg_primary = "bg-primary";
+    public static final String bg_primary_subtle = "bg-primary-subtle";
+    public static final String bg_secondary = "bg-secondary";
+    public static final String bg_secondary_subtle = "bg-secondary-subtle";
+    public static final String bg_success = "bg-success";
+    public static final String bg_success_subtle = "bg-success-subtle";
+    public static final String bg_danger = "bg-danger";
+    public static final String bg_danger_subtle = "bg-danger-subtle";
+    public static final String bg_warning = "bg-warning";
+    public static final String bg_warning_subtle = "bg-warning-subtle";
+    public static final String bg_info = "bg-info";
+    public static final String bg_info_subtle = "bg-info-subtle";
+    public static final String bg_light = "bg-light";
+    public static final String bg_light_subtle = "bg-light-subtle";
+    public static final String bg_dark = "bg-dark";
+    public static final String bg_dark_subtle = "bg-dark-subtle";
+    public static final String bg_body_secondary = "bg-body-secondary";
+    public static final String bg_body_tertiary = "bg-body-tertiary";
+    public static final String bg_body = "bg-body";
+    public static final String bg_black = "bg-black";
+    public static final String bg_white = "bg-white";
+    public static final String bg_transparent = "bg-transparent";
+
+    public static final String bg_gradient = "bg-gradient";
+
+    public static final String bg_opacity_75 = "bg-opacity-75";
+    public static final String bg_opacity_50 = "bg-opacity-50";
+    public static final String bg_opacity_25 = "bg-opacity-25";
+    public static final String bg_opacity_10 = "bg-opacity-10";
+
+}
