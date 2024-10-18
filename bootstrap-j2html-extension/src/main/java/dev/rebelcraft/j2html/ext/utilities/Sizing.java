6f0e2e701da08214ab19b13ecf572163d9f42ddd
teggr <robin@tegg.me.uk>
2024-10-18 08:48:52 +0100

    feat(boostrap) sizing

---
+++ bootstrap-j2html-extension/src/main/java/dev/rebelcraft/j2html/ext/utilities/Sizing.java
@@ -0,0 +1,25 @@
+package dev.rebelcraft.j2html.ext.utilities;
+
+public class Sizing {
+
+    public static final String w_25 = "w-25";
+    public static final String w_50 = "w-50";
+    public static final String w_75 = "w-75";
+    public static final String w_100 = "w-100";
+    public static final String w_auto = "w-auto";
+
+    public static final String h_25 = "h-25";
+    public static final String h_50 = "h-50";
+    public static final String h_75 = "h-75";
+    public static final String h_100 = "h-100";
+    public static final String h_auto = "h-auto";
+
+    public static final String mw_100 = "mw-100";
+    public static final String mh_100 = "mh-100";
+
+    public static final String min_vw_100 = "min-vw-100";
+    public static final String min_vh_100 = "min-vh-100";
+    public static final String vw_100 = "vw-100";
+    public static final String vh_100 = "vh-100";
+
+}
