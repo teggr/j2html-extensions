90579731feb393680725e3730fe2997e2ba3bf75
teggr <robin@tegg.me.uk>
2024-10-18 11:11:18 +0100

    feat(boostrap) visibility

---
+++ bootstrap-j2html-extension/src/main/java/dev/rebelcraft/j2html/ext/utilities/Visibility.java
@@ -0,0 +1,8 @@
+package dev.rebelcraft.j2html.ext.utilities;
+
+public class Visibility {
+
+    public static final String visible = "visible";
+    public static final String invisible = "invisible";
+
+}
