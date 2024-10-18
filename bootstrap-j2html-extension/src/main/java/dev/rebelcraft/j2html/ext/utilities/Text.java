2292b62fc0044cdea3956a94614074f8f43fdd3b
teggr <robin@tegg.me.uk>
2024-10-18 11:06:36 +0100

    feat(boostrap) text

---
+++ bootstrap-j2html-extension/src/main/java/dev/rebelcraft/j2html/ext/utilities/Text.java
@@ -0,0 +1,66 @@
+package dev.rebelcraft.j2html.ext.utilities;
+
+public class Text {
+
+    public static final String text_start = "text-start";
+    public static final String text_center = "text-center";
+    public static final String text_end = "text-end";
+
+    public static final String text_sm_start = "text-sm-start";
+    public static final String text_md_start = "text-md-start";
+    public static final String text_lg_start = "text-lg-start";
+    public static final String text_xl_start = "text-xl-start";
+    public static final String text_xxl_start = "text-xxl-start";
+
+    public static final String text_sm_center = "text-sm-center";
+    public static final String text_md_center = "text-md-center";
+    public static final String text_lg_center = "text-lg-center";
+    public static final String text_xl_center = "text-xl-center";
+    public static final String text_xxl_center = "text-xxl-center";
+
+    public static final String text_sm_end = "text-sm-end";
+    public static final String text_md_end = "text-md-end";
+    public static final String text_lg_end = "text-lg-end";
+    public static final String text_xl_end = "text-xl-end";
+    public static final String text_xxl_end = "text-xxl-end";
+
+    public static final String text_wrap = "text-wrap";
+    public static final String text_nowrap = "text-nowrap";
+
+    public static final String text_break = "text-break";
+
+    public static final String text_lowercase = "text-lowercase";
+    public static final String text_uppercase = "text-uppercase";
+    public static final String text_capitalize = "text-capitalize";
+
+    public static final String fs_1 = "fs-1";
+    public static final String fs_2 = "fs-2";
+    public static final String fs_3 = "fs-3";
+    public static final String fs_4 = "fs-4";
+    public static final String fs_5 = "fs-5";
+    public static final String fs_6 = "fs-6";
+
+    public static final String fw_bold = "fw-bold";
+    public static final String fw_bolder = "fw-bolder";
+    public static final String fw_semibold = "fw-semibold";
+    public static final String fw_medium = "fw-medium";
+    public static final String fw_normal = "fw-normal";
+    public static final String fw_light = "fw-light";
+    public static final String fw_lighter = "fw-lighter";
+    public static final String fst_italic = "fst-italic";
+    public static final String fst_normal = "fst-normal";
+
+    public static final String lh_1 = "lh-1";
+    public static final String lh_sm = "lh-sm";
+    public static final String lh_base = "lh-base";
+    public static final String lh_lg = "lh-lg";
+
+    public static final String font_monospace = "font-monospace";
+
+    public static final String text_reset = "text-reset";
+
+    public static final String text_decoration_underline = "text-decoration-underline";
+    public static final String text_decoration_line_through = "text-decoration-line-through";
+    public static final String text_decoration_none = "text-decoration-none";
+
+}
