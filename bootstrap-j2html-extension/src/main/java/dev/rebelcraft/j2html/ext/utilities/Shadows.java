ed0de69c2c25e419a1c8c6396ecd72a78860afe4
teggr <robin@tegg.me.uk>
2024-10-18 08:44:58 +0100

    feat(boostrap) shadows

---
+++ bootstrap-j2html-extension/src/main/java/dev/rebelcraft/j2html/ext/utilities/Shadows.java
@@ -0,0 +1,10 @@
+package dev.rebelcraft.j2html.ext.utilities;
+
+public class Shadows {
+
+    public static final String shadow_none = "shadow-none";
+    public static final String shadow_sm = "shadow-sm";
+    public static final String shadow = "shadow";
+    public static final String shadow_lg = "shadow-lg";
+
+}
