ee55f7dc6cabb34d5aaf73592ab3303d8e397e28
teggr <robin@tegg.me.uk>
2024-10-18 11:13:28 +0100

    feat(boostrap) zindex

---
+++ bootstrap-j2html-extension/src/main/java/dev/rebelcraft/j2html/ext/utilities/ZIndex.java
@@ -0,0 +1,11 @@
+package dev.rebelcraft.j2html.ext.utilities;
+
+public class ZIndex {
+
+    public static final String z_3 = "z-3";
+    public static final String z_2 = "z-2";
+    public static final String z_1 = "z-1";
+    public static final String z_0 = "z-0";
+    public static final String z_n1 = "z-n1";
+
+}
