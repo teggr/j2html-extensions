c2487233fe3e0cf5509c4322ab3fd718d784b48f
teggr <robin@tegg.me.uk>
2024-11-29 16:22:32 +0000

    feat(icon link) test

---
+++ bootstrap-j2html-extension/src/test/java/dev/rebelcraft/j2html/ext/helpers/IconLinkTest.java
@@ -0,0 +1,143 @@
+package dev.rebelcraft.j2html.ext.helpers;
+
+import dev.rebelcraft.j2html.ext.aria.AriaStatesAndProperties;
+import dev.rebelcraft.j2html.ext.icons.Icons;
+import dev.rebelcraft.uidocs.UiDocumentation;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+import static dev.rebelcraft.j2html.ext.ExtendedTagCreator.svg;
+import static dev.rebelcraft.j2html.ext.ExtendedTagCreator.use;
+import static j2html.TagCreator.a;
+import static j2html.TagCreator.text;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class IconLinkTest {
+
+    private UiDocumentation uiDocumentation;
+
+    @BeforeEach
+    void init(TestInfo testInfo) {
+        uiDocumentation = new UiDocumentation(testInfo);
+    }
+
+    @Test
+    void example() throws Exception {
+
+        String renderedHtml = uiDocumentation.render(
+                a()
+                        .withClasses(IconLink.icon_link)
+                        .withHref("#")
+                        .with(
+                                svg()
+                                        .withClasses(Icons.bi)
+                                        .attr(
+                                                AriaStatesAndProperties.ariaHidden("true")
+                                        )
+                                        .with(
+                                                use()
+                                                        .attr("xlink:href", "#box-seam")
+                                        ),
+                                text("Icon link")
+                        )
+        );
+
+        //language=HTML
+        assertEquals("""
+                <a class="icon-link" href="#">
+                  <svg class="bi" aria-hidden="true">
+                    <use xlink:href="#box-seam">
+                    </use>
+                  </svg>
+                  Icon link
+                </a>
+                """, renderedHtml);
+
+        // document
+        uiDocumentation.document("example", renderedHtml);
+
+        uiDocumentation.documentSource("example");
+
+    }
+
+    @Test
+    void example2() throws Exception {
+
+        String renderedHtml = uiDocumentation.render(
+                a()
+                        .withClasses(IconLink.icon_link)
+                        .withHref("#")
+                        .with(
+                                text("Icon link"),
+                                svg()
+                                        .withClasses(Icons.bi)
+                                        .attr(
+                                                AriaStatesAndProperties.ariaHidden("true")
+                                        )
+                                        .with(
+                                                use()
+                                                        .attr("xlink:href", "#arrow-right")
+                                        )
+                        )
+        );
+
+        //language=HTML
+        assertEquals("""
+                <a class="icon-link" href="#">
+                  Icon link
+                  <svg class="bi" aria-hidden="true">
+                    <use xlink:href="#arrow-right">
+                    </use>
+                  </svg>
+                </a>
+                """, renderedHtml);
+
+        // document
+        uiDocumentation.document("example2", renderedHtml);
+
+        uiDocumentation.documentSource("example2");
+
+    }
+
+    @Test
+    void styleOnHover() throws Exception {
+
+        String renderedHtml = uiDocumentation.render(
+                a()
+                        .withClasses(IconLink.icon_link,IconLink.icon_link_hover)
+                        .withHref("#")
+                        .with(
+                                text("Icon link"),
+                                svg()
+                                        .withClasses(Icons.bi)
+                                        .attr(
+                                                AriaStatesAndProperties.ariaHidden("true")
+                                        )
+                                        .with(
+                                                use()
+                                                        .attr("xlink:href","#arrow-right")
+                                        )
+                        )
+        );
+
+        //language=HTML
+        assertEquals("""
+        <a class="icon-link icon-link-hover" href="#">
+          Icon link
+          <svg class="bi" aria-hidden="true">
+            <use xlink:href="#arrow-right">
+            </use>
+          </svg>
+        </a>
+        """, renderedHtml);
+
+        // document
+        uiDocumentation.document("style-on-hover", renderedHtml);
+
+        uiDocumentation.documentSource("style-on-hover");
+
+    }
+
+
+}
